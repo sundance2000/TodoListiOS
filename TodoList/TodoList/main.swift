fa622948a7c0620e1d309004af88e84a35cbcdc6
Christian Oberdörfer <christian.michael.oberdoerfer@gmail.com>
2019-03-18 21:26:38 +0100

    Added QLog framework

---
+++ TodoList/TodoList/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  TodoList
+//
+//  Created by Christian Oberdörfer on 18.03.19.
+//  Copyright © 2019 Christian Oberdörfer. All rights reserved.
+//
+
+import QLog
+import UIKit
+
+// Genius solution from http://qualitycoding.org/app-delegate-for-tests/
+let appDelegateClass: AnyClass? = NSClassFromString("TodoListTests.TestAppDelegate") ?? AppDelegate.self
+let args = UnsafeMutableRawPointer(CommandLine.unsafeArgv).bindMemory(to: UnsafeMutablePointer<Int8>.self, capacity: Int(CommandLine.argc))
+_ = UIApplicationMain(CommandLine.argc, args, nil, NSStringFromClass(appDelegateClass!))
+
+func main() {
+    // QLog settings
+    QLog.loggers = [XcodeLogger(), FileLogger(), UiLogger.shared]
+
+}
---
+++ TodoList/TodoListTests/TestAppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  TestAppDelegate.swift
+//  TodoListTests
+//
+//  Created by Christian Oberdörfer on 18.03.19.
+//  Copyright © 2019 Christian Oberdörfer. All rights reserved.
+//
+
+import UIKit
+
+class TestAppDelegate: NSObject {
+
+    var window: UIWindow?
+
+}
